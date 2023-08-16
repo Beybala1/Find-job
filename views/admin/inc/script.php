8627a60b633afe4718b36459a23fe36a5cd692a6
Beybala Muxtarov <Beybala1>
2023-08-16 14:37:18 +0400

    Clear cached files and update .gitignore

---
+++ views/admin/inc/script.php
@@ -0,0 +1,12 @@
+
+<script src="admin/assets/vendor/libs/jquery/jquery.js"></script>
+<script src="admin/assets/vendor/libs/popper/popper.js"></script>
+<script src="admin/assets/vendor/js/bootstrap.js"></script>
+<script src="admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="admin/assets/vendor/libs/hammer/hammer.js"></script>
+<script src="admin/assets/vendor/libs/i18n/i18n.js"></script>
+<script src="admin/assets/vendor/libs/typeahead-js/typeahead.js"></script>
+<script src="admin/assets/vendor/js/menu.js"></script>
+<script src="admin/assets/vendor/libs/apex-charts/apexcharts.js"></script>
+<script src="admin/assets/js/main.js"></script>
+<script src="admin/assets/js/dashboards-analytics.js"></script>
