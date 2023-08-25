b52a611d360a9478bee046ed7ee276f428f29f99
Beybala Muxtarov <Beybala1>
2023-08-25 17:05:50 +0400

    PDO insert function

--- config/functions.php
+++ config/functions.php
@@ -1,4 +1,6 @@
 <?php
+
+//Get absolute path of the current working directory
 function asset($path): string
 {
     $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
@@ -6,6 +8,7 @@ function asset($path): string
     return $protocol . '://' . $host . '/' . ltrim($path, '/');
 }
 
+//Count the number of rows in a table
 function count_($table): void
 {
     global $con;
@@ -20,6 +23,7 @@ function count_($table): void
     echo $row_count;
  }
 
+ //Select data from a table
 function get($table): bool|array
 {
     global $con;
@@ -27,6 +31,81 @@ function get($table): bool|array
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+//Secure form data
+function csrf($input) {
+    return isset($input) ? trim(htmlspecialchars($input)) : '';
+}
+
+//Insert data into a table
+function insert($table, $data)
+{
+    // Additional validation: Ensure all fields are filled
+    global $con;
+
+    foreach ($data as $field => $value) {
+        if (empty($value)) {
+            return "All fields are required.";
+        }
+    }
+
+    // Prepare and execute the SQL query
+    $tableRow = implode(', ', array_keys($data));
+    $values = ':' . implode(', :', array_keys($data));
+
+    $sql = "INSERT INTO $table ($tableRow) VALUES ($values)";
+    $stmt = $con->prepare($sql);
+
+    try {
+        $stmt->execute($data);
+        echo "Data inserted successfully!";
+    } catch (PDOException $e) {
+        return "Error inserting data: " . $e->getMessage();
+    }
+}
+
+
+
+//function insert($table, $data)
+//{
+//    // Additional validation: Ensure all fields are filled
+//    if (empty($data)) {
+//        return "All fields are required.";
+//    }
+//
+//    // Additional validation: Check image file size (in bytes)
+//    $maxFileSize = 2 * 1024 * 1024; // 2 MB
+//    if ($data['image']['size'] > $maxFileSize) {
+//        return "Image file size exceeds the allowed limit.";
+//    }
+//
+//    // Handle image upload
+//    $uploadDirectory = 'uploads/';
+//    $uploadedFileName = $data['image']['name'];
+//    $image = $uploadDirectory . $uploadedFileName;
+//
+//    if (!move_uploaded_file($data['image']['tmp_name'], $image)) {
+//        return "Error uploading image.";
+//    }
+//
+//    // Additional validation: Check title length
+//    $maxTitleLength = 100; // For example, limit to 100 characters
+//    if (strlen($data['title']) > $maxTitleLength) {
+//        return "Title length exceeds the allowed limit.";
+//    }
+//
+//    // Insert data into the database
+//    $stmt = $con->prepare("INSERT INTO '$table' ($data)
+//                VALUES (:$data");
+//
+//    $stmt->bindParam(':title', $data['title']);
+//    try {
+//        $stmt->execute();
+//        return "Data inserted successfully!";
+//    } catch (PDOException $e) {
+//        return "Error inserting data: " . $e->getMessage();
+//    }
+//}
 
 
 
--- views/admin/news/create.php
+++ views/admin/news/create.php
@@ -21,80 +21,110 @@ require('../views/admin/inc/meta.php');
                             <h5 class="card-header">Blog yarat</h5>
                             <div class="card-body">
                                 <?php
+//                                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//                                    // Validate form data
+//                                    $title = $_POST['title'];
+//                                    $slug = $_POST['slug'];
+//                                    $alt = $_POST['alt'];
+//                                    $content = $_POST['content'];
+//
+//                                    // Additional validation: Ensure all fields are filled
+//                                    if (empty($title) || empty($slug) || empty($alt) || empty($content)) {
+//                                        die("All fields are .");
+//                                    }
+//
+//                                    // Additional validation: Check image file size (in bytes)
+//                                    $maxFileSize = 2 * 1024 * 1024; // 2 MB
+//                                    $uploadedFile = $_FILES['image']['tmp_name'];
+//                                    if ($_FILES['image']['size'] > $maxFileSize) {
+//                                        die("Image file size exceeds the allowed limit.");
+//                                    }
+//
+//                                    // Handle image upload
+//                                    $uploadDirectory = 'uploads/';
+//                                    $uploadedFileName = $_FILES['image']['name'];
+//                                    $image = $uploadDirectory . $uploadedFileName;
+//
+//                                    if (move_uploaded_file($uploadedFile, $image)) {
+//                                        // Image uploaded successfully
+//
+//                                        // Additional validation: Check title length
+//                                        $maxTitleLength = 100; // For example, limit to 100 characters
+//                                        if (strlen($title) > $maxTitleLength) {
+//                                            die("Title length exceeds the allowed limit.");
+//                                        }
+//
+//                                        // Insert data into the database
+//                                        $stmt = $con->prepare("INSERT INTO blogs (title, slug, alt, content, image)
+//                                                    VALUES (:title, :slug, :alt, :content, :image)");
+//
+//                                        $stmt->bindParam(':title', $title);
+//                                        $stmt->bindParam(':slug', $slug);
+//                                        $stmt->bindParam(':alt', $alt);
+//                                        $stmt->bindParam(':content', $content);
+//                                        $stmt->bindParam(':image', $image);
+//
+//                                        try {
+//                                            $stmt->execute();
+//                                            echo "Data inserted successfully!";
+//                                        } catch (PDOException $e) {
+//                                            echo "Error inserting data: " . $e->getMessage();
+//                                        }
+//                                    } else {
+//                                        echo "Error uploading image.";
+//                                    }
+//                                }
+                                if(isset($_POST['submit'])){
+                                    $title = csrf($_POST['title']);
+                                    $slug = csrf($_POST['slug']);
+                                    $alt = csrf($_POST['alt']);
+                                    $content = csrf($_POST['content']);
 
-                                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                                    // Validate form data
-                                    $title = $_POST['title'];
-                                    $slug = $_POST['slug'];
-                                    $alt = $_POST['alt'];
-                                    $content = $_POST['content'];
-
-                                    if (empty($title) || empty($slug) || empty($alt) || empty($content)) {
-                                        die("All fields are required.");
-                                    }
-
-                                    // Handle image upload
-                                    $uploadDirectory = 'uploads/';
-                                    $uploadedFile = $_FILES['image']['tmp_name'];
-                                    $uploadedFileName = $_FILES['image']['name'];
-                                    $image = $uploadDirectory . $uploadedFileName;
-
-                                    if (move_uploaded_file($uploadedFile, $image)) {
-                                        // Image uploaded successfully
-
-                                        // Insert data into the database
-                                        $stmt = $con->prepare("INSERT INTO blogs (title, slug, alt, content, image)
-                                                VALUES (:title, :slug, :alt, :content, :image)");
-
-                                        $stmt->bindParam(':title', $title);
-                                        $stmt->bindParam(':slug', $slug);
-                                        $stmt->bindParam(':alt', $alt);
-                                        $stmt->bindParam(':content', $content);
-                                        $stmt->bindParam(':image', $image);
-
-                                        try {
-                                            $stmt->execute();
-                                            echo "Data inserted successfully!";
-                                        } catch (PDOException $e) {
-                                            echo "Error inserting data: " . $e->getMessage();
-                                        }
-                                    } else {
-                                        echo "Error uploading image.";
-                                    }
+                                    // Call the insert function with the defined variables
+                                    $blog = insert(
+                                        'blogs',
+                                        [
+                                            'title' => $title,
+                                            'slug' => $slug,
+                                            'alt' => $alt,
+                                            'content' => $content,
+                                        ],
+                                    );
                                 }
+
                                 ?>
                                 <form action="#" method="POST" class="needs-validation" enctype="multipart/form-data" >
                                     <div class="mb-3">
                                         <label class="form-label" for="bs-validation-upload-file">Profile
                                             pic</label>
-                                        <input type="file" name="image" class="form-control" id="bs-validation-upload-file"
-                                               required value="1"/>
+                                        <input type="file" name="image" accept="image/*" class="form-control" id="bs-validation-upload-file"
+                                                value="1"/>
                                     </div>
                                     <div class="mb-3">
                                         <label class="form-label" for="bs-validation-title">Başlıq</label>
                                         <input type="text" name="title" class="form-control" id="bs-validation-title"
-                                               placeholder="John Doe" required />
+                                               placeholder="John Doe"  />
                                         <div class="valid-feedback"> Looks good! </div>
                                         <div class="invalid-feedback"> Başlıq daxil et. </div>
                                     </div>
                                     <div class="mb-3">
                                         <label class="form-label" for="bs-validation-slug">slug</label>
                                         <input type="text" name="slug" class="form-control" id="bs-validation-slug"
-                                               placeholder="John Doe" required />
+                                               placeholder="John Doe"  />
                                         <div class="valid-feedback"> Looks good! </div>
                                         <div class="invalid-feedback"> Please enter your slug. </div>
                                     </div>
                                     <div class="mb-3">
                                         <label class="form-label" for="bs-validation-alt">alt</label>
                                         <input type="text" name="alt" class="form-control" id="bs-validation-alt"
-                                               placeholder="John Doe" required />
+                                               placeholder="John Doe"  />
                                         <div class="valid-feedback"> Looks good! </div>
                                         <div class="invalid-feedback"> Please enter your alt. </div>
                                     </div>
                                     <div class="mb-3">
                                         <label class="form-label" for="bs-validation-bio">Kontent</label>
                                         <textarea class="form-control" id="bs-validation-bio"
-                                                  name="content" rows="3" required></textarea>
+                                                  name="content" rows="3" ></textarea>
                                     </div>
                                     <div class="row">
                                         <div class="col-12">
